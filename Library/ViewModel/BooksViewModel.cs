f0b24fd537eaf97585d1473bdd36ddf341eb85ad
Fernando Gonzaga <fernandooliv2107@gmail.com>
2025-12-04 09:06:19 -0300

    04/12 as 09:06

--- Library/ViewModel/BooksViewModel.cs
+++ Library/ViewModel/BooksViewModel.cs
@@ -11,18 +11,25 @@ public class BooksViewModel
 {
     private readonly BookService _bookService;
     private List<Books> _allBooks;
-    private int _curentIndex = 0;
-    private const int PageSize = 10;
+    private int _currentIndex = 0;
+    private const int PageSize = 10; // limite inicial
 
     public ObservableCollection<Books> BooksCollection { get; set; }
     public BooksViewModel()
     {
         _bookService = new BookService();
         _allBooks = _bookService.LoadBooks(); // carregando todos os livros
-        
-        
-        BooksCollection = new ObservableCollection<Books>(booksList);
+        BooksCollection = new ObservableCollection<Books>();
 
-
+        LoadNextPage();
+    }
+    public void LoadNextPage()
+    {
+        var nextBooks = _allBooks.Skip(_currentIndex).Take(PageSize);
+        foreach (var book in nextBooks)
+        {
+            BooksCollection.Add(book);
+        }
+        _currentIndex += PageSize;
     }
 }
--- Library/Views/BooksPainel.xaml.cs
+++ Library/Views/BooksPainel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -21,6 +22,18 @@ namespace Library.Views
         public BooksPainel()
         {
             InitializeComponent();
+        }
+
+        private void ListView_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var scrollViewer = e.OriginalSource as ScrollViewer;
+
+            // quando o user se aproxima do fim da fila, carrega mais livros
+            if (scrollViewer.VerticalOffset >= scrollViewer.ScrollableHeight - 50)
+            {
+                var vm = DataContext as BooksViewModel;
+                vm?.LoadNextPage();
+            }
         }
     }
 }
